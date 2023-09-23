6694e9056cc4e3264bcaee668d06417eb2ec5131
ashutosh <ashutoshsangra@gmail.com>
2023-09-23 09:14:54 +0530

    added login routes and jwt auth

--- serverside/routes/userlogin.js
+++ serverside/routes/userlogin.js
@@ -24,17 +24,18 @@ const authenticateJwt = async (req, res, next) => {
             next();
         });
     }
-    res.status(401).send({ message: "Authentication failed" });
+    else {
+        res.status(401).send({ message: "Authentication failed" });
+    }
 }
 
 const createToken = function (user) {
-    const token = jwt.sign(user.Email, SECRET_KEY, { expiresIn: '1h' });
+    const token = jwt.sign(user, SECRET_KEY, { expiresIn: '1h' });
     return token;
 }
 
 router.post('/signup', async (req, res) => {
     var user = {
-        UserId: generateUserId(),
         Email: req.body.userEmail,
         Password: req.body.pswd
     }
@@ -44,6 +45,9 @@ router.post('/signup', async (req, res) => {
         res.status(403).send({ message: "Email already registered" });
     }
     else {
+        const newUserId = await generateUserId();
+        console.log(newUserId);
+        user.UserId = newUserId;
         const newUser = new userModel(user);
         await newUser.save();
         const token = createToken(user);
@@ -55,14 +59,39 @@ router.post('/signup', async (req, res) => {
 
 });
 
+router.post('/login', async (req, res) => {
+    const user = {
+        Email: req.body.email,
+        Password: req.body.password
+    };
+    console.log(user);
+    const userInMongo = await userModel.findOne(user);
+
+    if (userInMongo) {
+        user.UserId = userInMongo.UserId;
+        const token = createToken(user);
+        res.send({ message: "Log In successfull", token: token });
+    }
+    else {
+        res.status(404).send({ message: "Incorrect username or password" });
+    }
+});
+
+router.post('/logout', authenticateJwt, (req, res) => {
+    const expireToken = jwt.sign({}, SECRET_KEY, { expiresIn: '0s' });
+
+    res.send({ message: "Logged out successfully", token: expireToken });
+});
+
 router.get('/hello', authenticateJwt, (req, res) => {
     res.send({ "message": "This is working" });
 });
 
 const generateUserId = async () => {
     const latestUser = await userModel.find().sort({ UserId: -1 }).limit(1);
-    if (latestUser) {
-        return latestUser.UserId + 1;
+    console.log(latestUser);
+    if (latestUser && latestUser.length > 0) {
+        return Number(latestUser[0].UserId) + 1;
     }
     return 1;
 }
